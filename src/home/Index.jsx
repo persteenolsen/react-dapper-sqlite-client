47433aee1fc022d6842726a6b6b9ce04c785cd3e
Per Olsen <persteen22@gmail.com>
2026-02-25 12:47:09 +0100

    Adjusted the Readme

--- src/home/Index.jsx
+++ src/home/Index.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div>
             <h2>React - CRUD Example with React Hook Form</h2>
-            <p>Last updated: 24-02-2026</p>
+            <p>Last updated: 25-02-2026</p>
             <p>List, add, edit and delete user records with React and the React Hook Form library</p>
             <p><Link to="users">&gt;&gt; Manage Users</Link></p>
         </div>
